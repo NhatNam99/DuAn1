619a4921a92e10e8df2cd39168edb97bb6a5da6c
Nhat Nam <phainit@yahoo.com.vn>
2018-11-26 00:24:02 +0700

    Update

--- app/src/main/java/vn/edu/poly/duan1/ChangePasswordActivity.java
+++ app/src/main/java/vn/edu/poly/duan1/ChangePasswordActivity.java
@@ -1,5 +1,7 @@
 package vn.edu.poly.duan1;
 
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,11 +10,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import vn.edu.poly.duan1.model.PatientManagement;
+
 public class ChangePasswordActivity extends AppCompatActivity {
 
     EditText edtpassw, edtpassnew, edtconfirm;
     SharedPreferences sharedPreferences;
-    Button btnssave;
+    Button btnssave, btnCanCel;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -24,28 +28,48 @@ public class ChangePasswordActivity extends AppCompatActivity {
         edtconfirm = findViewById(R.id.edtConfirm);
 
         btnssave = findViewById(R.id.btnSave);
+        btnCanCel = findViewById(R.id.btnCancel);
+        sharedPreferences = getSharedPreferences("config", Context.MODE_PRIVATE);
         btnssave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String pass = sharedPreferences.getString("Password", "");
                 String Pass = edtpassw.getText().toString();
+                String passnew = edtpassnew.getText().toString().trim();
+                String confirm = edtconfirm.getText().toString().trim();
+
+                if (Pass.isEmpty() || passnew.isEmpty() || confirm.isEmpty()) {
+                    if (Pass.isEmpty())
+                        edtpassw.setError(getString(R.string.notify_empty_user));
+                    if (pass.isEmpty())
+                        edtpassnew.setError(getString(R.string.notify_empty_pass));
+                    if (confirm.isEmpty())
+                        edtconfirm.setError(getString(R.string.notify_empty_pass));
+                }
 
                 if (Pass.equals(pass)) {
-                    String passnew = edtpassnew.getText().toString();
-                    String confirm = edtconfirm.getText().toString();
                     if (!confirm.equals(passnew)) {
                         Toast.makeText(ChangePasswordActivity.this, "Confirm Failed", Toast.LENGTH_SHORT).show();
                     } else {
                         SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.putString("PassNew", passnew);
+                        editor.putString("Password", passnew);
                         editor.commit();
                         Toast.makeText(ChangePasswordActivity.this, "Change Password Successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(ChangePasswordActivity.this, PatientManagementActivity.class);
+                        startActivity(intent);
                     }
                 } else {
                     Toast.makeText(ChangePasswordActivity.this, "Password Change Failed", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-        sharedPreferences = getSharedPreferences("config", 0);
+
+        btnCanCel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChangePasswordActivity.this, PatientManagementActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
